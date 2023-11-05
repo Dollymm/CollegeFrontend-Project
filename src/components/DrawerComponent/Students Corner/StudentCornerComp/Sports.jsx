17e10a1f07e82e9cd9e552610927d5ffff5ff1fe
Dollymm <100014305+Dollymm@users.noreply.github.com>
2023-11-05 17:42:15 +0530

    new commit

--- src/components/DrawerComponent/Students Corner/StudentCornerComp/Sports.jsx
+++ src/components/DrawerComponent/Students Corner/StudentCornerComp/Sports.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { StudentBox } from '../../Facilites/Style';
+import { SportsEvents } from '../../../../constant/StudentCornerImages';
+import { Box, Grid, Card, CardMedia } from '@mui/material';
 const Sports = () => {
   return (
-    <div>Sports</div>
+    <StudentBox>
+    <Grid container spacing={2}>
+      {SportsEvents.map((item, id) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+          <Box>
+            <CardMedia
+              component="img"
+              image={item.imagePath}
+              alt={`Cultural Image ${id}`}
+              style={{ height: 'auto',width:'50%',marginTop:'20px' }} // Adjust the height as needed
+            />
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  </StudentBox>
   )
 }
 
--- src/constant/StudentCornerImages.jsx
+++ src/constant/StudentCornerImages.jsx
@@ -29,3 +29,73 @@ export const CulturalImages = [
     imagePath: "./assets/Cultural9.jpg",
   },
 ];
+
+export const CollegeEvents=[
+  {
+    imagePath: "./assets/CollegeEvents1.jpg",
+  },
+  {
+    imagePath: "./assets/CollegeEvents2.jpg",
+  },
+  {
+    imagePath: "./assets/CollegeEvents3.jpg",
+  },
+  {
+    imagePath: "./assets/CollegeEvents4.jpg",
+  },
+  {
+    imagePath: "./assets/CollegeEvents5.jpg",
+  },
+  {
+    imagePath: "./assets/CollegeEvents6.jpg",
+  },
+]
+export const SportsEvents=[
+  {
+imagePath:'./assets/Sports1.jpg'
+  },
+  {
+imagePath:'./assets/sports2.jpg'
+  },
+  {
+imagePath:'./assets/sports3.jpg'
+  },
+  {
+imagePath:'./assets/Sports4.jpg'
+  },
+  {
+imagePath:'./assets/Sports5.jpg'
+  },
+  {
+imagePath:'./assets/sports6.jpg'
+  },
+  {
+imagePath:'./assets/Sports7.jpg'
+  },
+  {
+imagePath:'./assets/Sports8.jpg'
+  },
+  {
+imagePath:'./assets/Sports9.jpg'
+  },
+  {
+imagePath:'./assets/Sports10.jpeg'
+  },
+  {
+imagePath:'./assets/Sports11.jpeg'
+  },
+  {
+imagePath:'./assets/Sports12.jpeg'
+  },
+  {
+imagePath:'./assets/Sports13.jpeg'
+  },
+  {
+imagePath:'./assets/Sports14.jpeg'
+  },
+  {
+imagePath:'./assets/Sports15.jpeg'
+  },
+  
+
+]
