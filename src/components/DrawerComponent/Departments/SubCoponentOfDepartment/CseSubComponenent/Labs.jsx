35519c094cf4af5978e252db0a70e5197109342a
Dollymm <100014305+Dollymm@users.noreply.github.com>
2023-11-05 18:03:54 +0530

    new commit

--- src/components/DrawerComponent/Departments/SubCoponentOfDepartment/CseSubComponenent/Labs.jsx
+++ src/components/DrawerComponent/Departments/SubCoponentOfDepartment/CseSubComponenent/Labs.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { DepartmentBox, LabBox } from "../../Style";
-import Cprogramming from "./LabsCse/CProgramming";
+import Cprogramming from "./LabsCse/Cprogramming";
 import PythonProgramming from "./LabsCse/PythonProgramming";
 
 const Labs = () => {
